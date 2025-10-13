b0f729189ad2c773836dc674e998fca58dfe56a2
nephh <owenmccr@gmail.com>
2025-10-12 20:55:14 -1000

    implement basic preload script

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,14 +1,42 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { pollResources } from "./resourceManager.js";
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(app.getAppPath(), "dist-electron/preload.cjs"),
+    },
+  });
 
   if (process.env.NODE_ENV === "development") {
     mainWindow.loadURL("http://localhost:5173");
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
     console.log("Production mode");
   }
+}
+
+app.on("ready", createWindow);
+
+app.whenReady().then(() => {
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  pollResources();
 });
---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,49 @@
+import osUtils from "os-utils";
+import os from "os";
+import fs from "fs";
+
+const POLL_INTERVAL = 500; // in ms
+
+export function pollResources() {
+  setInterval(async () => {
+    const cpuUsage = ((await getCPUUsage()) * 100).toFixed(2) + "% CPU";
+    const ramUsage = (getRAMUsage() * 100).toFixed(2) + "% RAM";
+    const diskUsage = getDiskUsage();
+    console.log({ cpuUsage, ramUsage, diskUsage });
+  }, POLL_INTERVAL);
+}
+
+export function getStaticData() {
+  const totalStorage = getDiskUsage().total;
+  const cpuModel = os.cpus()[0].model;
+  const totalMem = Math.floor(os.totalmem() / 1_000_000_000) + " GB";
+  const platform = os.platform();
+
+  return {
+    totalStorage,
+    cpuModel,
+    totalMem,
+    platform,
+  };
+}
+
+function getCPUUsage(): Promise<number> {
+  return new Promise((resolve) => {
+    osUtils.cpuUsage(resolve);
+  });
+}
+
+function getRAMUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getDiskUsage() {
+  const stats = fs.statfsSync(process.platform === "win32" ? "C:\\" : "/");
+  const total = stats.blocks * stats.bsize;
+  const free = stats.bfree * stats.bsize;
+  return {
+    total: Math.floor(total / 1_000_000_000) + " GB", // in GB
+    usage: ((1 - free / total) * 100).toFixed(2) + "%", // as a percentage
+    gbUsed: Math.floor((total - free) / 1_000_000_000) + " GB", // in GB
+  };
+}
